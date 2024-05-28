39a174a01600cce09b068edf032ec54aee3a6933
Douglas C. Santos <crowbellx@outlook.com>
2024-05-28 15:11:45 -0300

    feat:Criando a camada de serviço

---
+++ src/main/java/com/crowbellx/todo_list/service/TodoServices.java
@@ -0,0 +1,39 @@
+package com.crowbellx.todo_list.service;
+
+import com.crowbellx.todo_list.entity.Todo;
+import com.crowbellx.todo_list.repository.TodoRepository;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TodoServices {
+    private TodoRepository todoRepository;
+
+    public TodoServices(TodoRepository todoRepository) {
+        this.todoRepository = todoRepository;
+    }
+
+    public List<Todo> create(Todo todo){
+        todoRepository.save(todo);
+        return list();
+    }
+
+    public List<Todo> list(){
+       var sort = Sort.by("prioridade").descending()
+               .and(Sort.by("nome").ascending());
+
+       return todoRepository.findAll(sort);
+    }
+
+    public List<Todo> update(Todo todo){
+        todoRepository.save(todo);
+        return list();
+    }
+
+    public List<Todo> delete(Long id){
+        todoRepository.deleteById(id);
+        return list();
+    }
+}
