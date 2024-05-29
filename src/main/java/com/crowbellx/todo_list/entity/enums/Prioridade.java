f30500fe45742b45615f4e729588abe31b42cdda
Douglas C. Santos <crowbellx@outlook.com>
2024-05-29 10:18:59 -0300

    feat:alterando o enum prioridade

--- src/main/java/com/crowbellx/todo_list/entity/enums/Prioridade.java
+++ src/main/java/com/crowbellx/todo_list/entity/enums/Prioridade.java
@@ -1,12 +1,6 @@
 package com.crowbellx.todo_list.entity.enums;
 
 public enum Prioridade {
-    URGENTE(1),
-    NORMAL(2);
-
-    public final int descricao;
-
-    Prioridade(int descricao){
-        this.descricao = descricao;
-    }
+    URGENTE,
+    NORMAL;
 }
